d3b8bd452dc3f8a74fef9ba87507978095d56db4
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-08 18:17:37 +0600

    Update main.c

--- SPL-Lab_Assignment-Another Group/main.c
+++ SPL-Lab_Assignment-Another Group/main.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
 
+void printFloors(int currentFloor, int targetFloor, FILE *output) {
+  if (currentFloor > targetFloor) {
+    return;
+  }
+
+  fprintf(output, "I've reached the %d", currentFloor);
+
+  if (currentFloor == 1) {
+    fprintf(output, "st");
+  } else if (currentFloor == 2) {
+    fprintf(output, "nd");
+  } else if (currentFloor == 3) {
+    fprintf(output, "rd");
+  } else {
+    fprintf(output, "th");
+  }
+
+  fprintf(output, " floor\n");
+
+  printFloors(currentFloor + 1, targetFloor, output);
+}
+
+int main() {
+  FILE *inputFile = fopen("input.txt", "w");
+  if (inputFile == NULL) {
+    printf("Error opening input file.\n");
+    return 1;
+  }
+
+  int n;
+  printf("Enter the number of floors: ");
+  scanf("%d", &n);
+  fprintf(inputFile, "%d", n);
+  fclose(inputFile);
+
+  FILE *output = fopen("output.txt", "w");
+  if (output == NULL) {
+    printf("Error opening output file.\n");
+    return 1;
+  }
+
+  printFloors(1, n, output);
+
+  fclose(output);
+
+  return 0;
+}

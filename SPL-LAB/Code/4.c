4b09d2f66312f5489827b2a18357db1b4015a7eb
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-20 13:09:50 +0600

    Create 4.c

---
+++ SPL-LAB/Code/4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int integer = 5;
+  float floating = 3.141593;
+  char character = 'a';
+  printf("The integer value:%d\n", integer);
+  printf("the floating point value:%f\n", floating);
+  printf("The character value:%c\n", character);
+  return 0;
+}

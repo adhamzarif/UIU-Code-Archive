d421364c23c3853e284b04398c10d9e342a9b163
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-20 16:13:25 +0600

    Create 3.Pattern_Based_On_Input.c

---
+++ SPL-LAB/Class 4(Nested Loop)/Code/3.Pattern_Based_On_Input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  int i, j, n;
+  scanf("%d", &n);
+  for (i = 1; i <= n; i++) {
+    int a = i;
+    for (j = 1; j <= i; j++) {
+      printf("%d ", a);
+      a++;
+    }
+    printf("\n");
+  }
+  return 0;
+}

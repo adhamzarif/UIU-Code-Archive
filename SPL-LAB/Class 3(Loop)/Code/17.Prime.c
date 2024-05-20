0d69fe80df7a66251cd6fbb1daf22b29910c58ec
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-20 15:17:50 +0600

    Create 17.Prime.c

---
+++ SPL-LAB/Class 3(Loop)/Code/17.Prime.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int num;
+  scanf("%d", &num);
+  int flag = 1;
+  for (int i = 2; i < num; i++) {
+    if (num % i == 0) {
+      flag = 0;
+      break;
+    }
+  }
+  if (flag == 1) {
+    printf("Prime");
+  } else {
+    printf("Not Prime");
+  }
+  return 0;
+}

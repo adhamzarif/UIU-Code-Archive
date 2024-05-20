a0c58b30ea6760761032a1d0ff52c263f0b22591
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-05-20 14:36:22 +0600

    Create 3.c

---
+++ SPL-LAB/Class 2/Code/3.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void) {
+  int num;
+  printf("Enter a number: ");
+  scanf( "%d", &num);
+  if(num == 0)
+  {
+    printf("Zero");
+  }
+  if(num == 1)
+  {
+    printf("One");
+  }
+  if(num == 2)
+  {
+    printf("Two");
+  }
+  if(num == 3)
+  {
+    printf("Three");
+  }
+  if(num == 4)
+  {
+    printf("Four");
+  }
+  if(num == 5)
+  {
+    printf("Five");
+  }
+  if(num == 6)
+  {
+    printf("Six");
+  }
+  if(num == 7)
+  {
+    printf("Seven");
+  }
+  if(num == 8)
+  {
+    printf("Eight");
+  }
+  if(num == 9)
+  {
+    printf("Nine");
+  }
+  return 0;
+}

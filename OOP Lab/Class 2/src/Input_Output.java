b8dd64586c099281d33e7022978f2d03f6cbcac4
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-06-09 22:10:15 +0600

    Backup From ArchLinux

---
+++ OOP Lab/Class 2/src/Input_Output.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input_Output {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter Your Name: ");
+        String s;
+        s = sc.nextLine();
+        System.out.println(s);
+    }
+}

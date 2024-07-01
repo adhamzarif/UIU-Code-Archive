cd62be69de9edda69fa9310ba7f440cce3de5beb
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-07-01 15:13:06 +0600

    Update Main.java

--- OOP Lab/Class 3/BoxInfo/Main.java
+++ OOP Lab/Class 3/BoxInfo/Main.java
@@ -4,8 +4,13 @@ public class Main {
     Box box1 = new Box(2.0, 3.0, 4.0);
     Box box2 = new Box(5.0, 6.0, 7.0);
 
+//double ans1 = box1.getVolume();
+//double ans2 = box2.getVolume();
+
     // Calculate and print the volume of the boxes
     System.out.println("Volume of box1: " + box1.getVolume());
     System.out.println("Volume of box2: " + box2.getVolume());
+
+
   }
 }

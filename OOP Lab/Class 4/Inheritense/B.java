c224e9be62e14dd27db0ed0f2490f435ea1e319d
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-07-06 23:55:11 +0600

    Backup From ArchLinux

---
+++ OOP Lab/Class 4/Inheritense/B.java
@@ -0,0 +1,12 @@
+package Inheritense;
+
+public class B extends A{
+        public B(int id) {
+            super(id);
+        }
+
+        void displayB() {
+            System.out.println("Inside B Class" + " " + "ID: " + id);
+        }
+    }
+

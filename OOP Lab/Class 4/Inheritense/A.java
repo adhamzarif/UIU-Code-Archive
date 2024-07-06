25a4647b72167758190ae8bd462dc0b5218b77fa
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-07-06 23:54:15 +0600

    Backup From ArchLinux

---
+++ OOP Lab/Class 4/Inheritense/A.java
@@ -0,0 +1,13 @@
+package Inheritense;
+
+public class A {
+    int id;
+
+    A(int id) {
+        this.id = id;
+    }
+
+    void displayA() {
+        System.out.println("Ïnside A Class" + " " + "ID: " + id);
+    }
+}

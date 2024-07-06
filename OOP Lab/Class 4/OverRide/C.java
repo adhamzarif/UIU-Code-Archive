0e7a40a0d6aa8ddea0739f66bce373962d3244dc
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-07-06 23:58:01 +0600

    Backup From ArchLinux

---
+++ OOP Lab/Class 4/OverRide/C.java
@@ -0,0 +1,11 @@
+package OverRide;
+
+public class C extends B{
+        public C(int id) {
+            super(id);
+        }
+
+        void displayC() {
+            System.out.println("Inside C Class" + " " + "ID: " + id);
+        }
+    }

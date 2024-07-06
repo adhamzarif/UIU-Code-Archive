cdeee3fe16a0f7e1bf189eda73329bcdaa1ce1b9
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-07-07 00:40:04 +0600

    Backup From ArchLinux

---
+++ OOP Lab/Lab Exercises/Lab 2/Bank Info/BankAccount.java
@@ -0,0 +1,37 @@
+package Lab2.BankInfo;
+
+    public class BankAccount {
+        String uName;
+        String holdingNumber;
+        int deposit = 0;
+
+        // Constructor Creation
+        public BankAccount(String uName, String holdingNumber) {
+            this.uName = uName;
+            this.holdingNumber = holdingNumber;
+        }
+
+        // Method Creation
+        public int depositMoney(int amount) {
+            if (amount > 0) {
+                deposit += amount;
+            } else {
+                System.out.println("Please Enter a positive number!!");
+            }
+            return deposit;
+        }
+
+        public int withdrawMoney(int amount) {
+            if (deposit > 0 && amount <= deposit) {
+                deposit -= amount;
+            } else {
+                System.out.println("Please enter a valid number!!");
+            }
+            return deposit;
+        }
+
+        public int currentBalance() {
+            return deposit;
+        }
+    }
+

667141e14f3e5957f41425929783197ec5675c7b
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-07-06 23:57:06 +0600

    Backup From ArchLinux

---
+++ OOP Lab/Class 4/OverRide/A.java
@@ -0,0 +1,13 @@
+package OverRide;
+
+public class A {
+    int id;
+
+    A(int id) {
+        this.id = id;
+    }
+
+    void displayA() {
+        System.out.println("Ïnside A Class" + " " + "ID: " + id);
+    }
+}

95ae1ad0546c9d65e5fdb8bd488dadf0086b3f3a
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-07-06 23:56:07 +0600

    Backup From ArchLinux

---
+++ OOP Lab/Class 4/Inheritense/Main.java
@@ -0,0 +1,10 @@
+package Inheritense;
+
+public class Main {
+    public static void main(String[] args) {
+        C c = new C(1234);
+        c.displayA();
+        c.displayB();
+        c.displayC();
+    }
+}

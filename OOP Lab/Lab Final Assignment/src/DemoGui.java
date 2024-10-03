956ed2ac1e7308f21b6f52a73fda427f128935a1
Adham Zarif <zarifseam52@gmail.com>
2024-10-03 12:06:26 +0600

    Backup From ArchLinux On 03-10-2024 03:30 AM

---
+++ OOP Lab/Lab Final Assignment/src/DemoGui.java
@@ -0,0 +1,192 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+class Product {
+    String name;
+    int price;
+    int stock;
+
+    Product(String name, int price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+}
+
+class CartItem {
+    Product product;
+    int quantity;
+
+    CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    int getTotalPrice() {
+        return product.price * quantity;
+    }
+}
+
+public class DemoGui extends JFrame {
+    private JList<String> list1;
+    private JList<String> cartList;
+    private JButton addToCartButton;
+    private JLabel productLabel;
+    private JPanel mainPanel;
+    private JTextField quantityField;
+    private JButton incrementButton;
+    private JButton decrementButton;
+    private JButton checkoutButton;
+    private JButton deleteButton;
+    private JLabel totalAmountLabel;
+
+    ArrayList<Product> products = new ArrayList<>();
+    DefaultListModel<String> productListModel = new DefaultListModel<>();
+    DefaultListModel<String> cartListModel = new DefaultListModel<>();
+    ArrayList<CartItem> cart = new ArrayList<>();
+    int selectedProductIndex = -1;
+
+    public DemoGui() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setContentPane(mainPanel);
+
+        quantityField.setText("0");
+
+        products.add(new Product("Rice", 123, 100));
+        products.add(new Product("Sugar", 456, 150));
+        products.add(new Product("Salt", 789, 200));
+
+        for (Product p : products) {
+            productListModel.addElement("Name: " + p.name + " | Price: " + p.price + " | Stock: " + p.stock);
+        }
+        list1.setModel(productListModel);
+
+        list1.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent me) {
+                if (me.getClickCount() == 1) {
+                    selectedProductIndex = list1.getSelectedIndex();
+                    if (selectedProductIndex != -1) {
+                        Product selectedProduct = products.get(selectedProductIndex);
+                        productLabel.setText("Selected Product: " + selectedProduct.name);
+                    }
+                }
+            }
+        });
+
+        decrementButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int currentQuantity = Integer.parseInt(quantityField.getText());
+                    if (currentQuantity > 0) {
+                        quantityField.setText(String.valueOf(--currentQuantity));
+                    }
+                } catch (NumberFormatException ex) {
+                    quantityField.setText("0");
+                }
+            }
+        });
+
+        incrementButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int currentQuantity = Integer.parseInt(quantityField.getText());
+                    quantityField.setText(String.valueOf(++currentQuantity));
+                } catch (NumberFormatException ex) {
+                    quantityField.setText("1");
+                }
+            }
+        });
+
+        addToCartButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (selectedProductIndex != -1) {
+                    Product selectedProduct = products.get(selectedProductIndex);
+                    int selectedQuantity = 0;
+                    try {
+                        selectedQuantity = Integer.parseInt(quantityField.getText());
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(DemoGui.this, "Please enter a valid quantity.");
+                        return;
+                    }
+
+                    if (selectedQuantity > selectedProduct.stock) {
+                        JOptionPane.showMessageDialog(DemoGui.this, "Not enough stock available!");
+                    } else {
+                        boolean productInCart = false;
+                        for (CartItem item : cart) {
+                            if (item.product.name.equals(selectedProduct.name)) {
+                                item.quantity += selectedQuantity;
+                                productInCart = true;
+                                break;
+                            }
+                        }
+                        if (!productInCart) {
+                            CartItem newItem = new CartItem(selectedProduct, selectedQuantity);
+                            cart.add(newItem);
+                        }
+
+                        updateCartList();
+                        selectedProduct.stock -= selectedQuantity;
+                        productListModel.set(selectedProductIndex, "Name: " + selectedProduct.name + " | Price: " + selectedProduct.price + " | Stock: " + selectedProduct.stock);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(DemoGui.this, "Please select a product.");
+                }
+            }
+        });
+
+        deleteButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int selectedCartIndex = cartList.getSelectedIndex();
+                if (selectedCartIndex != -1) {
+                    CartItem selectedCartItem = cart.get(selectedCartIndex);
+                    Product product = selectedCartItem.product;
+                    product.stock += selectedCartItem.quantity;
+
+                    cart.remove(selectedCartIndex);
+                    updateCartList();
+                    productListModel.set(products.indexOf(product), "Name: " + product.name + " | Price: " + product.price + " | Stock: " + product.stock);
+                } else {
+                    JOptionPane.showMessageDialog(DemoGui.this, "Please select an item from cart to delete.");
+                }
+            }
+        });
+
+        checkoutButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String summary = "";
+                int totalAmount = 0;
+                int totalItems = 0;
+
+                for (CartItem item : cart) {
+                    int productTotal = item.getTotalPrice();
+                    summary = summary + "Product: " + item.product.name + " | Qty: " + item.quantity + " | Total: " + productTotal + "\n";
+                    totalAmount += productTotal;
+                    totalItems += item.quantity;
+                }
+
+                totalAmountLabel.setText("Total Items: " + totalItems + " | Total Amount: " + totalAmount);
+            }
+        });
+
+        setVisible(true);
+    }
+
+    private void updateCartList() {
+        cartListModel.clear();
+        for (CartItem item : cart) {
+            cartListModel.addElement("Product: " + item.product.name + " | Quantity: " + item.quantity + " | Price: " + (item.product.price * item.quantity));
+        }
+        cartList.setModel(cartListModel);
+    }
+
+    public static void main(String[] args) {
+        new DemoGui();
+    }
+}
---
+++ OOP Lab/OOP Lab Final Assignment/OOP Lab Final Assignment/src/V1/DemoGui.java
@@ -0,0 +1,98 @@
+package V1;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+class Product {
+    String name;
+    int price;
+//    private String grade;
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }}
+
+public class DemoGui extends JFrame{
+    private JList list1;
+    private JButton button1;
+    private JLabel label1;
+    private JPanel jP;
+    private JLabel label2;
+    private JLabel label3;
+    private JTextField textField1;
+    private JTextField a0TextField;
+    private JButton addButton;
+    private JTextField textField3;
+    private JLabel label4;
+    private JButton doneButton1;
+    int inx=-1;
+    public DemoGui() {
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500,300);
+        setLocationRelativeTo(null);
+        setLayout(null);
+
+        setContentPane(jP);
+
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product("Rice", 123));
+        products.add(new Product("Sugar", 456));
+        products.add(new Product("Salt", 789));
+
+        DefaultListModel<String> l1 = new DefaultListModel<>();
+
+        for (Product s : products) {
+            String price = String.valueOf(s.price);
+            l1.addElement("Name: "+ s.name+" "+"Price: "+price);
+        }
+        list1.setModel(l1);
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(list1.getSelectedIndex() != -1){
+                    inx = list1.getSelectedIndex();
+                    System.out.println(list1.getSelectedIndex());
+                    textField1.setText(list1.getSelectedValue().toString());
+                }else
+                JOptionPane.showMessageDialog(DemoGui.this,"Please select a valid Product");
+            }
+        });
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int productQuantity = Integer.parseInt(a0TextField.getText());
+                productQuantity+=1;
+                a0TextField.setText(String.valueOf(productQuantity));
+            }
+        });
+
+        doneButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(Integer.parseInt(a0TextField.getText()) != 0) {
+                    if(inx != -1){
+                        String pName = products.get(inx).name;
+                        int pPrice = products.get(inx).price;
+                        int tCount = Integer.parseInt(a0TextField.getText());
+                        int tPrice = pPrice * tCount;
+                        textField3.setText("Name: "+pName+"Total Price: "+tPrice);
+                    }else{
+                        textField3.setText("No Product Added");
+                    }
+                }
+                else
+                    textField3.setText("No Product Added!!!");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new DemoGui();
+    }
+}
+

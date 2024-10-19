c84d3534de58d0ff49b43022bde7643a165959e1
Adham Zarif <148647705+adhamzarif@users.noreply.github.com>
2024-10-19 13:44:35 +0600

    Update UIU-Code-Archive/ICS directory

--- UIU-Code-Archive/ICS/242-Code/print function 2.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main ()
-{
-   printf("Hello World!\n ") ;
-    printf("Welcome to C programming.\n ") ;
-     printf("This is going to be fun!\n ") ;
-    return 0;
-}
--- UIU-Code-Archive/ICS/242-Code/variables and arithmetic operations 5.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-   float a=2.3,b=5.8,c=1.1,d=3.5;
-   printf("output is=%f\n", ((a + b - c) * ((d - a) / d)));
-    return 0;
-}
